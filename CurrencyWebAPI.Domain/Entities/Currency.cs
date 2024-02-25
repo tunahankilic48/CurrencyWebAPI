280831dea17034a59d7dc9b9436372f4e9b81f9c
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-02-25 13:49:31 +0300

    Currency and Currency detail entities were created.

---
+++ CurrencyWebAPI.Domain/Entities/Currency.cs
@@ -0,0 +1,10 @@
+﻿namespace CurrencyWebAPI.Domain.Entities
+{
+    public class Currency
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? AttributeName { get; set; }
+
+    }
+}
