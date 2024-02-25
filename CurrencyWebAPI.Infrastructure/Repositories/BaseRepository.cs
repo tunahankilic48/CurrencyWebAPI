232e14cd1fdae8553a4ef219ec2cd303070a7a38
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-02-25 22:24:54 +0300

    Currency Controlelr was added.

--- CurrencyWebAPI.Infrastructure/Repositories/BaseRepository.cs
+++ CurrencyWebAPI.Infrastructure/Repositories/BaseRepository.cs
@@ -31,6 +31,7 @@ namespace CurrencyWebAPI.Infrastructure.Repositories
 
         public async Task<bool> Delete(TEntity entity)
         {
+             _table.Remove(entity);
             return await Save() > 0;
         }
 
