949127ed49510334b5f24a2184b9dabc5058b6ab
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-02-25 13:50:20 +0300

    Base, Currency and Currency detail repositories were created.

---
+++ CurrencyWebAPI.Domain/Repositories/ICurrencyDetailRepository.cs
@@ -0,0 +1,8 @@
+﻿using CurrencyWebAPI.Domain.Entities;
+
+namespace CurrencyWebAPI.Domain.Repositories
+{
+    public interface ICurrencyDetailRepository : IBaseRepository<CurrencyDetail>
+    {
+    }
+}
