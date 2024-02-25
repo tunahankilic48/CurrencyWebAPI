743c3be111c1efbb3c6d884d403b6de565c08c25
Tunahan Kılıç <tunahankilic942@gmail.com>
2024-02-25 17:44:21 +0300

    Web API layer configurations with other layers were done.

---
+++ CurrencyWebAPI.Infrastructure/Migrations/20240225142758_First.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CurrencyWebAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class First : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Currencies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "VARCHAR(20)", nullable: false),
+                    AttributeName = table.Column<string>(type: "VARCHAR(20)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Currencies", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "CurrencyDetails",
+                columns: table => new
+                {
+                    CurrencyId = table.Column<int>(type: "int", nullable: false),
+                    Date = table.Column<DateTime>(type: "DATETIME", nullable: false),
+                    Value = table.Column<string>(type: "VARCHAR(10)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CurrencyDetails", x => new { x.CurrencyId, x.Date });
+                    table.ForeignKey(
+                        name: "FK_CurrencyDetails_Currencies_CurrencyId",
+                        column: x => x.CurrencyId,
+                        principalTable: "Currencies",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CurrencyDetails");
+
+            migrationBuilder.DropTable(
+                name: "Currencies");
+        }
+    }
+}
